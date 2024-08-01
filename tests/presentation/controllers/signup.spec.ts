bc3abb62cc79b196ce7b05ca8d52a8c9ad66682d
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-31 18:12:17 -0700

    test: ensure SignUpController returns body Error if no name correct is provided

--- tests/presentation/controllers/signup.spec.ts
+++ tests/presentation/controllers/signup.spec.ts
@@ -4,13 +4,30 @@ describe('SignUp Controller', () => {
   it('should return 400 if no code invitation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
-      passwordConfirmation: 'any_password'
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: code invitation'))
+  })
+
+  it('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        codeInvitation: 'any_code',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
