d5e9c1092046696f53dc2e335b93362358226125
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-03 12:45:22 -0700

    test: ensure SignUpController returns 500 if CreateAccount throws

--- tests/presentation/controllers/signup.spec.ts
+++ tests/presentation/controllers/signup.spec.ts
@@ -182,4 +182,20 @@ describe('SignUp Controller', () => {
       password: 'any_password'
     })
   })
+
+  it('should return 500 if CreateAccount throws', () => {
+    const { sut, createAccountStub } = makeSut()
+    jest.spyOn(createAccountStub, 'create').mockImplementationOnce(() => { throw new Error() })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
