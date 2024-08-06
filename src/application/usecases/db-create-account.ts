1fde0e979537f94b47eb1cfdc116c39a19048b68
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-06 15:51:14 -0700

    feat: ensure DbCreateAccount calls Encrypter with correct password

---
+++ src/application/usecases/db-create-account.ts
@@ -0,0 +1,12 @@
+import { type AccountModel } from '@/domain/models'
+import { type CreateAccountModel } from '@/domain/usecases'
+import { type Encrypter } from '../protocols'
+
+export class DbCreateAccount {
+  constructor (private readonly encrypter: Encrypter) { }
+
+  async create (account: CreateAccountModel): Promise<AccountModel | null> {
+    await this.encrypter.encrypt(account.password)
+    return new Promise(resolve => { resolve(null) })
+  }
+}
