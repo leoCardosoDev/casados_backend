b38137892916f9c888b8dd17ffffe231c6e8312a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-06 16:17:36 -0700

    test: ensure DbCreateAccount throws if Encrypter throws

--- tests/application/usecases/db-create-account.spec.ts
+++ tests/application/usecases/db-create-account.spec.ts
@@ -36,4 +36,16 @@ describe('DbCreateAccount Usecases', () => {
     await sut.create(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+  // Teste para garantir que caso o encrypter retorne uma Exception, que seja tratada por quem chamou e não aqui
+  it('should throws if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockRejectedValueOnce(new Error())
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const promise = sut.create(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
