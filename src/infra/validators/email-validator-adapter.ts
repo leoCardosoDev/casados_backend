1bdd84a724df60f2827430923da47235e5381f9c
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-05 18:24:21 -0700

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/infra/validators/email-validator-adapter.ts
@@ -0,0 +1,7 @@
+import { type EmailValidator } from '@/validation/protocols'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return false
+  }
+}
