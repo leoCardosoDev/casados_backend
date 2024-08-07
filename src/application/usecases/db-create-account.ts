9f7abe10652faa0b35a7c98084e6f0a0bded4fa9
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-07 07:27:08 -0700

    feat: ensure DbAccount returns an account on success

--- src/application/usecases/db-create-account.ts
+++ src/application/usecases/db-create-account.ts
@@ -1,16 +1,16 @@
 import { type AccountModel } from '@/domain/models'
-import { type CreateAccountModel } from '@/domain/usecases'
+import { type CreateAccount, type CreateAccountModel } from '@/domain/usecases'
 import { type CreateAccountRepository, type Encrypter } from '@/application/protocols'
 
-export class DbCreateAccount {
+export class DbCreateAccount implements CreateAccount {
   constructor (
     private readonly encrypter: Encrypter,
     private readonly createAccountRepository: CreateAccountRepository
   ) { }
 
-  async create (accountData: CreateAccountModel): Promise<AccountModel | null> {
+  async create (accountData: CreateAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    await this.createAccountRepository.create(Object.assign({}, accountData, { password: hashedPassword }))
-    return new Promise(resolve => { resolve(null) })
+    const account = await this.createAccountRepository.create(Object.assign({}, accountData, { password: hashedPassword }))
+    return account
   }
 }
