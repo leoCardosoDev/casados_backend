071ba2a417decd91b182809a748894e6658300a2
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-08 06:34:04 -0700

    refactor: move sut create to a helper method

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -7,18 +7,21 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const salt = 12
+const makeSut = (): BcrypterAdapter => {
+  return new BcrypterAdapter(salt)
+}
+
 describe('Bcrypt Adapter', () => {
   it('should call brcypt with correct value', async () => {
-    const salt = 12
-    const sut = new BcrypterAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   it('should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcrypterAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hashed_value')
   })
